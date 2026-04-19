58fc9eee1f6d351b74da9828691569addada57e3
Almaz <Almazshaehov777@gmail.com>
2026-04-19 15:27:10 +0300

    добавлено модальное окно с данными при нажатии на аниме. Немного улучшен дизайн

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ const animeData = [
         episodes: 12,
         imagePath: 'AnimeImagePath/kaoruHanaWa.jpg'
     },
+    {
+        id: 3,
+        title: 'Атака Титанов',
+        rating: 9.3,
+        year: 2013,
+        episodes: 87,
+        imagePath: 'AnimeImagePath/attackOnTitan.jpg'
+    },
 ];
 
 function renderCard(){
@@ -24,7 +32,7 @@ function renderCard(){
     for(let i = 0; i<animeData.length; ++i){
         const anime = animeData[i];
         html += `
-    <div class="card">
+    <div class="card" data-id="${anime.id}">
     <div class="info">
     <img src="${anime.imagePath}" alt="${anime.title}">
     <div class="cardInfo">
@@ -32,13 +40,61 @@ function renderCard(){
             <div class="details">
                 <span class="rating">${anime.rating}</span>
                 <br>
-                <span>${anime.year} • ${anime.episodes} эп.</span>
+                <span class="year">${anime.year} • ${anime.episodes} эп.</span>
             </div>
         </div>
         </div>
     </div>`;
     }
     container.innerHTML = html;
+    
 }
 
 renderCard();
+
+const con = document.getElementById('animeGrid');
+const modal = document.getElementById('modalOverlay');
+const closeBtn = document.getElementById('closeBtn');
+const modalWin = document.getElementById('modalWindow');
+
+con.onclick = (e) => {
+    const card = e.target.closest('.card')
+    if(card){
+        const animeId = parseInt(card.dataset.id);
+        const selectAnime = animeData.find(anime => anime.id === animeId);
+
+        if(selectAnime){
+            fillModal(selectAnime);
+            modal.classList.add('show');
+        }
+    }
+}
+
+modal.onclick = (e) => {
+    if(e.target === modal){
+        modal.classList.remove('show');
+    }
+};
+
+function fillModal(anime){
+    modalWin.innerHTML = `
+    <div class="modalHeader">
+        ${anime.title}
+        <button id="buttonClose">X</button>
+    </div>
+    <div class="modalMain">
+        <p class="animeInfo">Рейтинг: ${anime.rating}★</p>
+        <p class="animeInfo">Эпизоды: ${anime.episodes} эп.</p>
+        <p class="animeInfo">Год: ${anime.year}</p>
+    </div>
+    <div class="modalFooter">
+        <button class="buttonDemo">Смотреть DEMO</button>
+    </div>`
+
+    const btnClose = document.getElementById('buttonClose');
+
+    if(btnClose){
+        btnClose.onclick = () => modal.classList.remove('show');
+    }
+}
+
